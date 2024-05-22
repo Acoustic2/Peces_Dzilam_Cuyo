b97192471c0efe2df7674c3d1bcf7558392a2f7e
Emilio_Gp <garpe.emilio19@gmail.com>
2024-05-22 12:18:05 -0600

    k

--- Cambio 1.R
+++ Cambio 1.R
@@ -1,21 +1,29 @@
 #Base de datos 
 #Quarto guide
 
+library(vegan)
+library(ggplot2)
+
 datos <- read.csv("Peces_Dzilam.csv")
 
 #Realizar un MDS para compocisión de especies de peces 
 #MATRIZ DE DISTANCIA 
  
-datos_numeros <- datos[5:15]
+datos_numeros <- datos[5:19]
   
-dist_datos <- dist(datos_numeros)
+dist_datos <- vegdist(sqrt(datos_numeros), method = "bray")
 
-mds_result <- isoMDS(dist_datos, k = 2)
+mds_result <- metaMDS(dist_datos, k = 2)
 
-plot(mds_result[, 1], mds_result[, 2], 
-     xlab = "Dimensión 1", ylab = "Dimensión 2", main = "MDS No Métrico")
+# plot(mds_result)
 
- 
+mds_points <-as.data.frame(mds_result$points)
+
+mds <- cbind(datos[,1:4], mds_points)
+
+ggplot(mds, aes(x = MDS1, y = MDS2))+
+  geom_point(aes(colour = Sitio, shape = Hora), size = 4)+
+  theme_bw()
 
 
-#Análisis de riqueza potencial en ambos sitios 
+&#Análisis de riqueza potencial en ambos sitios 
