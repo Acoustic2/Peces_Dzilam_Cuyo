f701909a4087444e85ca62322cb159f4f7387034
Emilio_Gp <garpe.emilio19@gmail.com>
2024-05-23 11:17:39 -0600

    k

--- Cambio 1.R
+++ Cambio 1.R
@@ -3,13 +3,14 @@
 
 library(vegan)
 library(ggplot2)
+library(dplyr)
 
 datos <- read.csv("Peces_Dzilam.csv")
 
 #Realizar un MDS para compocisión de especies de peces 
 #MATRIZ DE DISTANCIA 
  
-datos_numeros <- datos[5:19]
+datos_numeros <- datos[6:20]
   
 dist_datos <- vegdist(sqrt(datos_numeros), method = "bray")
 
@@ -19,11 +20,26 @@ mds_result <- metaMDS(dist_datos, k = 2)
 
 mds_points <-as.data.frame(mds_result$points)
 
-mds <- cbind(datos[,1:4], mds_points)
+mds <- cbind(datos[,1:5], mds_points)
 
 ggplot(mds, aes(x = MDS1, y = MDS2))+
-  geom_point(aes(colour = Sitio, shape = Hora), size = 4)+
+  geom_point(aes(colour = Sitio, shape = Hora, size = Abundancia))+
   theme_bw()
 
+#Análisis de riqueza potencial en ambos sitios 
 
-&#Análisis de riqueza potencial en ambos sitios 
+library(vegan)
+library(betapart)
+
+
+datos_beta <- read.csv("Beta.csv")
+data.sp<-Beta_div_art[,2:14]
+data.sp<-as.data.frame(data.sp)
+
+beta.m<-beta.multi(data.sp, index.family="jaccard")
+beta.c<-betapart.core(data.sp)
+beta.multi(beta.c, index.family="jaccard")
+beta.m.sor<-beta.multi(data.sp, index.family="sorensen")
+beta.c.sor<-betapart.core(data.sp)
+beta.multi(beta.c.sor, index.family="sorensen")
+
